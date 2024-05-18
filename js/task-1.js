cef0bc41c4d019d506af27f4cf310185ebadc15e
Kate <159571202+turchynska@users.noreply.github.com>
2024-05-18 06:52:37 -0400

    update

--- js/task-1.js
+++ js/task-1.js
@@ -170,3 +170,43 @@ function calculateTotal(number) {
 console.log(calculateTotal(2));
 */
 
+/*
+Напиши функцію isEnoughCapacity(products, containerSize), яка обчислює, чи помістяться всі товари в контейнер при пакуванні.
+
+Функція оголошує два параметри:
+
+products — об’єкт, у якому ключі містять назви товарів, а їхні значення — кількість цих товарів. Наприклад, { apples: 2, grapes: 4 }.
+containerSize — число, максимальна кількість одиниць товарів, яку в себе може вмістити контейнер.
+Функція має повернути результат перевірки, чи помістяться всі товари в контейнер. Тобто порахувати загальну
+ кількість товарів в об’єкті products і повернути true, якщо вона менше або дорівнює containerSize, і false, якщо ні.
+function isEnoughCapacity(products, containerSize) {
+    let sum = 0;
+    const values = Object.values(products)
+    for (const value of values) {
+        sum += value;
+    }
+    return sum <= containerSize;
+}
+
+console.log(
+    isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)
+  ); // true
+  
+  console.log(
+    isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12)
+  ); // false
+  
+  console.log(
+    isEnoughCapacity({ apples: 1, lime: 5, tomatoes: 3 }, 14)
+  ); // true
+  
+  console.log(
+    isEnoughCapacity({ apples: 18, potatoes: 5, oranges: 2 }, 7)
+  ); // false
+  */
+ /*
+ Напиши функцію calcAverageCalories(days), яка повертає середньодобове значення кількості калорій, які спортсмен споживав протягом тижня.
+  Функція очікує один параметр: days — масив об’єктів. Кожен об’єкт описує день тижня та кількість калорій calories, спожитих спортсменом, 
+  у цей день. Візьми код нижче і встав після оголошення своєї функції для перевірки коректності її роботи. 
+  У консоль будуть виведені результати її викликів.*/ 
+ 
