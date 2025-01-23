05239f8eef8bc7c5b02fca1f52792fbd0756d590
Kate <159571202+turchynska@users.noreply.github.com>
2025-01-23 10:38:13 -0500

    object module4

--- js/hm1-6.js
+++ js/hm1-6.js
@@ -458,134 +458,87 @@
 // console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
 
 
+//////////Module 4
+//object
+// const book = {
+//   title: 'The Last Kingdom',
+//   author: "Bernard Cornwell",
+//   genres: ["historical prose", 'adventure'],
+//   rating: 8.38,
+// };
+// for (const key in book) {
+//   console.log(key);
+//   console.log(book[key])
+// }
+
+//ObjectValues
+// const book = {
+//   title: "The Last Kingdom",
+//   author: "Bernard Cornwell",
+//   rating: 8.38
+// };
+// const keys = Object.keys(book);
+// console.log(keys)
+
+// const values = Object.values(book);
+// console.log(values)
+
+// const books = [
+//   { title: "The last Kingdom", author: "Bernard Cornwell", rating: 8.2 },
+//   { title: "Beside still water", author: "Robert Sheckley", rating: 9 },
+//   { title: "The dream of a ridiculous Man", author: "Fyydor Dosotoevsky", rating: 3 }
+// ];
+// const titles = [];
+// let totalRating = 0;
+// for (const book of books) {
+//   totalRating += book.rating;
+//   titles.push(book.title)
+// }
+// console.log(titles)
+// console.log(totalRating)
+
+//МЕТОДИ ОБЄКТУ
+// const obj = {
+//   method(value) {
+//     console.log(`Im a method with ${value}`)
+//   }
+// }
+// obj.method(5)
 
 
+// const bookShelf = {
+//   books: ["The Last Kingdom", "Dream Guardian"],
+//   getBooks() {
+//     return "Returning all books";
+//   },
+//   addBook(bookName) {
+//     return `Adding book ${bookName}`
+//   }
+// }
+// console.log(bookShelf.getBooks())
+// console.log(bookShelf.addBook("New book 1"))
 
+//this
+const bookShelf = {
+  books: ["The Last Kingdom"],
 
-
-
-
-/*
-function makeTransaction(quantity, pricePerDroid, customerCredits){
-    let total = quantity * pricePerDroid;toy
-    if( total > customerCredits){con
-      return "Insufficient funds!";
-    } 
-     return `You ordered ${quantity} droids worth ${total} credits!`
-    }
-    console.log(makeTransaction(5, 3000, 23000)); // "You ordered 5 droids worth 15000 credits!"
-    console.log(makeTransaction(3, 1000, 15000)); // "You ordered 3 droids worth 3000 credits!"
-    console.log(makeTransaction(10, 5000, 8000)); // "Insufficient funds!"
-    console.log(makeTransaction(8, 2000, 10000)); // "Insufficient funds!"
-    console.log(makeTransaction(10, 500, 5000)); // "You ordered 10 droids worth 5000 credits!"
-    */
-    
-    /*
-    function formatMessage(message, maxLength){
-      if (message <= maxLength){
-        return message;
-      }
-      return `${message.slice(0, maxLength)}`;
-    }
-    
-    console.log(formatMessage("Curabitur ligula sapien", 16)); // "Curabitur ligula..."
-    console.log(formatMessage("Curabitur ligula sapien", 23)); // "Curabitur ligula sapien"
-    console.log(formatMessage("Vestibulum facilisis purus nec", 20)); // "Vestibulum facilisis..."
-    console.log(formatMessage("Vestibulum facilisis purus nec", 30)); // "Vestibulum facilisis purus nec"
-    console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 15)); // "Nunc sed turpis..."
-    console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 41)); // "Nunc sed turpis a felis in nunc fringilla
-    */
-    
-    /*
-    function checkForSpam(message){
-    if (message.toLowerCase().includes(`spam`) || message.toLowerCase().includes(`sale`)){
-      return true;
-    }
-    return false;
-    }
-    
-    console.log(checkForSpam("Latest technology news")); // false
-    console.log(checkForSpam("JavaScript weekly newsletter")); // false
-    console.log(checkForSpam("Get best sale offers now!")); // true
-    console.log(checkForSpam("Amazing SalE, only tonight!")); // true
-    console.log(checkForSpam("Trust me, this is not a spam message")); // true
-    console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
-    console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
-    
-    
-    function getShippingCost(country) {
-      switch (country) {
-        case `China`:
-          return `Shipping to ${country} will cost ${100} credits`;
-        case `Chile`:
-          return `Shipping to ${country} will cost ${250} credits`;
-        case `Australia`:
-          return `Shipping to ${country} will cost ${170} credits`;
-        case `Jamaica `:
-          return `Shipping to ${country} will cost ${120} credits`;
-      }
-    }
-    console.log(getShippingCost("Australia")); // "Shipping to Australia will cost 170 credits"
-    console.log(getShippingCost("Germany")); // "Sorry, there is no delivery to your country"
-    console.log(getShippingCost("China")); // "Shipping to China will cost 100 credits"
-    console.log(getShippingCost("Chile")); // "Shipping to Chile will cost 250 credits"
-    console.log(getShippingCost("Jamaica")); // "Shipping to Jamaica will cost 120 credits"
-    console.log(getShippingCost("Sweden")); // "Sorry, there is no delivery to your country"
-    */
-
-
-
-
-
-
-////// go-it 3 модуль / домашня //////
-/*
-function slugify(title){
-let slug = title.split(" ").join("-")
-
-return slug.toLowerCase();
+  getBooks() {
+    return this.books
+  },
+  addBook(BookName) {
+    this.books.push(bookName)
+  }
 }
+console.log(bookShelf.getBooks())
 
 
-console.log(slugify("Arrays for begginers")); // "arrays-for-begginers"
-console.log(slugify("English for developer")); // "english-for-developer"
-console.log(slugify("Ten secrets of JavaScript")); // "ten-secrets-of-javascript"
-console.log(slugify("How to become a JUNIOR developer in TWO WEEKS")); // "how-to-become-a-junior-developer-in-two-weeks"
-*/
 
-/*
-function makeArray(firstArray, secondArray, maxLength){
-    let newArray = firstArray.concat(secondArray);
-    if( newArray.length < maxLength){
-    }
-return newArray.slice(0, maxLength)
 
-}
 
-console.log(makeArray(["Mango", "Poly"], ["Ajax", "Chelsea"], 3)); // ["Mango", "Poly", "Ajax"]
-console.log(makeArray(["Mango", "Poly", "Houston"], ["Ajax", "Chelsea"], 4)); // ["Mango", "Poly", "Houston", "Ajax"]
-console.log(makeArray(["Mango"], ["Ajax", "Chelsea", "Poly", "Houston"], 3)); // ["Mango", "Ajax", "Chelsea"]
-console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 2)); // ["Earth", "Jupiter"]
-console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 4)); // ["Earth", "Jupiter", "Neptune", "Uranus"]
-console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus", "Venus"], 0)); // []
-*/
-/*
-function filterArray(numbers, value) {
-let newNumbers = [];
-for( let i = 0; i <= numbers.length; i ++){
-    if(numbers[i] > value){
-        newNumbers.push(numbers[i])
-    }
-}
-return newNumbers;
-}
 
-console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
-console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
-console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
-console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
-console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76];
-*/
+
+
+
 
 ////////-----hm-4----//////////
 /*
